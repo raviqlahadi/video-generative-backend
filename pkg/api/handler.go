b3933287b20be2191cff48100691a89b533a36cd
raviqlahadi <raviq.lahadi@laku6.com>
2024-11-17 23:49:27 +0700

    add healthcheck

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -1,12 +1,21 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+// HealthCheckHandler returns a basic status message to check if the server is running
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Server is up and running!")
+}
+
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
-
+	router.HandleFunc("/", TrendingHanler).Methods("GET")
 	router.HandleFunc("/trending", TrendingHanler).Methods("GET")
 	router.HandleFunc("/category", CategoryHandler).Methods("GET")
 	router.HandleFunc("/search", SearchHandler).Methods("GET")
